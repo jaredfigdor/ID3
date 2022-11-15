dd593d850b854a3e0702cf307e04c51bacc49736
jaredfigdor <jaredfigdor@gmail.com>
2022-11-15 03:06:10 -0500

    com

--- public/index.js
+++ public/index.js
@@ -91,4 +91,14 @@ window.addEventListener('DOMContentLoaded', () => {
     await timeout(1000);
     runFooter();
   })();
+
+
+  var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
 });
